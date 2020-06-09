49ef27b0f1ae1736438811767d630f7c3898f346
Jonatan Machado <jonatanmachado35@gmail.com>
2020-06-09 12:12:10 -0300

    configuração inicial pronta

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
-//
-//
-// ATENÇÃO: Não é necessário mexer neste arquivo.
-// Novos componentes devem ir dentro do componente AppContainer
-//
-//
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import React from 'react'
-import JssProvider from 'react-jss/lib/JssProvider'
-import { create } from 'jss'
-import { MuiThemeProvider, createGenerateClassName, jssPreset } from '@material-ui/core/styles'
-import { createMuiTheme } from '@material-ui/core/styles'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import { AppContainer } from './components/AppContainer'
-
-const generateClassName = createGenerateClassName()
-const jss = create({
-	...jssPreset(),
-	insertionPoint: document.getElementById('jss-insertion-point'),
-})
-
-const theme = createMuiTheme()
+import GlobalStyle from './styles/global';
+import Routes from './routes';
 
 function App() {
-	return (
-		<JssProvider jss={jss} generateClassName={generateClassName}>
-			<MuiThemeProvider theme={theme}>
-				<CssBaseline />
-				<AppContainer />
-			</MuiThemeProvider>
-		</JssProvider>
-	)
+  return (
+    <BrowserRouter>
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
