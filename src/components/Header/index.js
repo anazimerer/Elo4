8594d38194f2b44ae7d38259a1441f25c53b807f
Jonatan Machado <jonatanmachado35@gmail.com>
2020-06-09 13:52:47 -0300

    ajuste nas rotas

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 function Header() {
-  return <div />;
+  return <Container />;
 }
 
 export default Header;
