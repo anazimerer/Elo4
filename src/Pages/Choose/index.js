690539d81e105e1eaa7c9699806aacfb76998cc3
Ronaldo Jonson <ronaldojonson@hotmail.com>
2020-06-09 23:08:42 -0300

    Local-Storage_Styling

--- src/Pages/Choose/index.js
+++ src/Pages/Choose/index.js
@@ -2,23 +2,48 @@ import React from 'react';
 
 import {Link} from 'react-router-dom'
 
-import { Container, HeaderDiv, MainDiv, FooterDiv, Option_Button  } from './styles';
+import Button from '@material-ui/core/Button';
+
+import { Container, HeaderDiv, MainDiv, FooterDiv, BackgroundImage, Logo  } from './styles';
+import Crafting from "../../assets/images/Crafting.jpg"
+import elo4 from "../../assets/images/elo4.png"
 
 function Choose() {
+  const [role, setRole] = React.useState(
+    localStorage.getItem('role') || ''
+  );
+
+  React.useEffect(() => {
+    switch(role)
+    {
+      case "BUYER" : 
+        //go to other page;
+        break;
+      case "SELLER" : 
+        //go to other page;
+        break;
+      default:
+        //go to other page;
+        break;
+    }
+  }, []);
+
+  const onSetRoleEvent = (roleClicked) => {
+    localStorage.setItem('role', roleClicked);
+    setRole(roleClicked);
+  }
+  
   return (
     <Container>
-      <HeaderDiv />
-
+      <BackgroundImage src={Crafting}/>
       <MainDiv>
         <Link to="/">
-          <Option_Button>Quero Comprar</Option_Button>
+          <Button variant="contained" color="primary" size="large" onClick={() => onSetRoleEvent("BUYER")}>Quero Comprar</Button>
         </Link>
         <Link to="/">
-          <Option_Button onClick="">Quero Vender</Option_Button>
+          <Button variant="contained" color="primary" size="large" onClick={() => onSetRoleEvent("SELLER")}>Quero Vender</Button>
         </Link>
       </MainDiv>
-
-      <FooterDiv />
     </Container>
   )
 }
--- src/Pages/Choose/styles.js
+++ src/Pages/Choose/styles.js
@@ -2,29 +2,16 @@ import styled from 'styled-components';
 
 export const Container = styled.div``;
 
-export const HeaderDiv = styled.header`
-  background-color: blue;
-  height: 10vh;
-`;
-
-export const FooterDiv = styled.footer`
-  background-color: blue;
-  height: 10vh;
-`;
+export const BackgroundImage = styled.img`
+  position: absolute;
+  left:0;
+  height: 100%;
+  width: 100%;
+`
 
 export const MainDiv = styled.main`
-  background-color: black;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  height: 80vh;
-`;
-
-export const Option_Button = styled.button`
-  background-color: purple;
-  height: 10vh;
-  width: 30vw;
-  &:nth-child(2) {
-    background-color: green;
-  }
+  height: 90vh;
 `;
