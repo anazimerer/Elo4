5a8ae9321396193b26740d34131aeb8ddbd6d589
edupezz <edupezz@gmail.com>
2020-06-10 12:27:18 -0300

    Pagina AddProducts - primeira manipulacao.

---
+++ src/components/AddProducts/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const AddProductContainer = styled.div `
+
+    width: 100vw;
+    background-color: lightsalmon;
+    display: flex;
+`
+export const AddProductInputContainer = styled.div `
+
+   width: 80vw;
+   display: flex;
+   flex-flow: column;
+   background-color: green;
+`
