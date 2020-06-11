119610d3aab7610c201a08a4a9c01b478890b21b
Jonatan Machado <jonatanmachado35@gmail.com>
2020-06-11 12:01:00 -0300

    router atualizado

--- src/Pages/Solo_Product/index.js
+++ src/Pages/Solo_Product/index.js
@@ -1,90 +1,99 @@
-import React, {useEffect} from 'react';
-import api from '../../service/api'
+import React, { useEffect } from 'react';
+import api from '../../service/api';
 
 //import { Container } from './styles';
 
-function Solo_Product() {
-    const [product, setProduct] = React.useState(
-    
-    );
+function Solo_Product({
+  match: {
+    params: { id },
+  },
+}) {
+  const [product, setProduct] = React.useState();
 
-    useEffect(() => {
-        GetProductlist();
-    }, []);
+  useEffect(() => {
+    GetProductlist();
+  }, []);
 
-    const GetProductlist = () => {
-        api.get()
-        .then((response) => {
-            console.log(response.data.products[0]);
-            setProduct(response.data.products[0]);
-        })
+  const GetProductlist = () => {
+    api.get().then((response) => {
+      console.log(response.data.products[0]);
+      setProduct(response.data.products[0]);
+    });
+  };
+
+  const GetProductInfo = () => {
+    if (product !== undefined) {
+      return (
+        <div>
+          <div>
+            <h2>{product.name}</h2>
+            <p>{product.category}</p>
+          </div>
+          <div>
+            <p>{product.description}</p>
+          </div>
+          <div>
+            <img src="https://picsum.photos/20/20" />
+            <p>Leticia</p>
+            <p>Araraquara, SP</p>
+            <button>Seguir Loja</button>
+          </div>
+          <div>
+            <p>{product.installments}</p>
+            <p>Disponiveis</p>
+            <p>80</p>
+            <p>Vendidos</p>
+            <p>15</p>
+            <p>Recomendações</p>
+          </div>
+        </div>
+      );
     }
+  };
 
-    const GetProductInfo = () => {
-        if(product !== undefined){
-            return <div>
-                <div>
-                    <h2>{product.name}</h2>
-                    <p>{product.category}</p>
-                </div>
-                <div>
-                    <p>{product.description}</p>
-                </div>
-                <div>
-                    <img src="https://picsum.photos/20/20"/>
-                    <p>Leticia</p>
-                    <p>Araraquara, SP</p>
-                    <button>Seguir Loja</button>
-                </div>
-                <div>
-                    <p>{product.installments}</p>
-                    <p>Disponiveis</p>
-                    <p>80</p>
-                    <p>Vendidos</p>
-                    <p>15</p>
-                    <p>Recomendações</p>
-                </div>
-            </div> 
-        }
+  const GetPhotos = () => {
+    if (product !== undefined) {
+      return (
+        <div>
+          <img src="https://picsum.photos/200/200" />
+          <img src="https://picsum.photos/200/200" />
+          <img src="https://picsum.photos/200/200" />
+          <img src="https://picsum.photos/200/200" />
+          <img src="https://picsum.photos/200/200" />
+        </div>
+      );
     }
+  };
 
-    const GetPhotos = () => {
-        if(product !== undefined){
-            return <div>
-                <img src="https://picsum.photos/200/200"/>
-                <img src="https://picsum.photos/200/200"/>
-                <img src="https://picsum.photos/200/200"/>
-                <img src="https://picsum.photos/200/200"/>
-                <img src="https://picsum.photos/200/200"/>
-            </div> 
-        }
+  const GetPrice = () => {
+    if (product !== undefined) {
+      return (
+        <div>
+          <div>
+            <p>{product.price}</p>
+            <button>Quero Comprar</button>
+            <button>Adicionar ao Carrinho</button>
+          </div>
+          <div>
+            <img src="https://picsum.photos/20/20" />
+            <p>Comprar na Elo4 é seguro</p>
+            <img src="https://picsum.photos/20/20" />
+            <p>Se não gostar a devolução é gratis</p>
+            <img src="https://picsum.photos/20/20" />
+            <p>Denunciar</p>
+          </div>
+        </div>
+      );
     }
+  };
 
-    const GetPrice = () => {
-        if(product !== undefined){
-            return <div>
-                <div>
-                    <p>{product.price}</p>
-                    <button>Quero Comprar</button>
-                    <button>Adicionar ao Carrinho</button>
-                </div>
-                <div>
-                    <img src="https://picsum.photos/20/20"/>
-                    <p>Comprar na Elo4 é seguro</p>
-                    <img src="https://picsum.photos/20/20"/>
-                    <p>Se não gostar a devolução é gratis</p>
-                    <img src="https://picsum.photos/20/20"/>
-                    <p>Denunciar</p>
-                </div>
-            </div> 
-        }
-    }
-
-  return <div>
+  return (
+    <div>
       {GetProductInfo()}
       {GetPhotos()}
       {GetPrice()}
-  </div>;
+    </div>
+  );
 }
 
 export default Solo_Product;
--- src/components/AddProducts/index.js
+++ src/components/AddProducts/index.js
@@ -1,164 +1,181 @@
 import React, { useState } from 'react';
 import { Formulario } from './styles';
-import { 
-    Grid, 
-    Button,
-    makeStyles,
-    TextField,
-    Container,
-}
-from '@material-ui/core';
+import {
+  Grid,
+  Button,
+  makeStyles,
+  TextField,
+  Container,
+} from '@material-ui/core';
 
 import api from '../../service/api';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      marginTop: 25,
-    },
-    textField: {
-      marginLeft: theme.spacing(2),
-      marginRight: theme.spacing(2),
-    },
-    margin: {
-        marginLeft: theme.spacing(2),
-        marginTop: 4,
-    }
-  }));
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginTop: '90px',
+  },
+  textField: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+  },
+  margin: {
+    marginLeft: theme.spacing(2),
+    marginTop: 4,
+  },
+}));
 
-export default function AddProducts () {
-    
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [price, setPrice] = useState('');
-    const [paymentMethod, setPaymentMethod] = useState('');
-    const [category, setCategory] = useState('');
-    const [photos, setPhotos] = useState('');
-    const [installments, setInstallments] = useState('');
+export default function AddProducts() {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [paymentMethod, setPaymentMethod] = useState('');
+  const [category, setCategory] = useState('');
+  const [photos, setPhotos] = useState('');
+  const [installments, setInstallments] = useState('');
 
-    const ClearAll = () =>{
+  const ClearAll = () => {
+    setName('');
+    setDescription('');
+    setPrice('');
+    setPaymentMethod('');
+    setCategory('');
+    setPhotos('');
+    setInstallments('');
+  };
 
-        setName('')
-        setDescription('');
-        setPrice('')
-        setPaymentMethod('');
-        setCategory('');
-        setPhotos('');
-        setInstallments('');
-    }
+  const CreateProduct = () => {
+    const body = {
+      name: name,
+      description: description,
+      price: price,
+      paymentMethod: paymentMethod,
+      category: category,
+      photos: [photos],
+      installments: installments,
+    };
 
-    const CreateProduct = () => {
-        const body = {
-            name: name,
-            description: description,
-            price: price,
-            paymentMethod: paymentMethod,
-            category: category,
-            photos: [photos],
-            installments: installments
-        }
+    api
+      .post('', body)
+      .then(() => {
+        window.alert('Produto cadastrado com sucesso');
+        ClearAll();
+      })
+      .catch((error) => {
+        window.alert('Erro ao cadastrar produto');
+        ClearAll();
+      });
+  };
 
-        api.post('', body,)
-          .then(() => {
-            window.alert('Produto cadastrado com sucesso');
-            ClearAll();
-          }).catch(error => {
-              window.alert('Erro ao cadastrar produto');
-              ClearAll();
-          });}
+  const classes = useStyles();
 
-          const classes = useStyles();
+  return (
+    <Container maxWidth="sm">
+      <Formulario className={classes.root}>
+        <Grid item xs={12}>
+          <TextField
+            value={name}
+            fullWidth
+            style={{ margin: 8 }}
+            variant="outlined"
+            placeholder="Nome do produto"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Grid>
 
-    return(
-        <Container maxWidth="sm">
-            <Formulario className={classes.root}>
-                <Grid item xs={12}>
-                    <TextField value={name}
-                        fullWidth
-                        style={{ margin: 8 }}
-                        variant="outlined"
-                        placeholder="Nome do produto"
-                        onChange={ (e) => setName(e.target.value) }
-                    />
-                </Grid>
+        <Grid item xs={12}>
+          <TextField
+            value={description}
+            fullWidth
+            style={{ margin: 8 }}
+            variant="outlined"
+            placeholder="Descrição do produto"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </Grid>
 
-                <Grid item xs={12}>
-                    <TextField value={description}
-                        fullWidth
-                        style={{ margin: 8 }}
-                        variant="outlined"
-                        placeholder="Descrição do produto"
-                        onChange={ (e) => setDescription(e.target.value) }
-                    />
-                </Grid>
+        <Grid item xs={12}>
+          <TextField
+            value={price}
+            fullWidth
+            style={{ margin: 8 }}
+            variant="outlined"
+            placeholder="Preço do produto"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </Grid>
 
-                <Grid item xs={12}>
-                    <TextField value={price}
-                        fullWidth
-                        style={{ margin: 8 }}
-                        variant="outlined"
-                        placeholder="Preço do produto"
-                        onChange={ (e) => setPrice(e.target.value) }
-                    />
-                </Grid>
+        <Grid item xs={12}>
+          <TextField
+            value={paymentMethod}
+            fullWidth
+            style={{ margin: 8 }}
+            variant="outlined"
+            placeholder="Forma de pagamento"
+            onChange={(e) => setPaymentMethod(e.target.value)}
+          />
+        </Grid>
 
-                <Grid item xs={12}>
-                    <TextField value={paymentMethod}
-                        fullWidth
-                        style={{ margin: 8 }}
-                        variant="outlined"
-                        placeholder="Forma de pagamento"
-                        onChange={ (e) => setPaymentMethod(e.target.value) }
-                    />
-                </Grid>
+        <Grid item xs={12}>
+          <TextField
+            value={category}
+            fullWidth
+            style={{ margin: 8 }}
+            variant="outlined"
+            placeholder="Categoria do produto"
+            onChange={(e) => setCategory(e.target.value)}
+          />
+        </Grid>
 
-                <Grid item xs={12}>
-                    <TextField value={category}
-                        fullWidth
-                        style={{ margin: 8 }}
-                        variant="outlined"
-                        placeholder="Categoria do produto"
-                        onChange={ (e) => setCategory(e.target.value) }
-                    />
-                </Grid>
+        <Grid item xs={12}>
+          <TextField
+            value={photos}
+            fullWidth
+            style={{ margin: 8 }}
+            variant="outlined"
+            placeholder="URL da foto do produto"
+            onChange={(e) => setPhotos(e.target.value)}
+          />
+        </Grid>
 
-                <Grid item xs={12}>
-                    <TextField value={photos}
-                        fullWidth
-                        style={{ margin: 8 }}
-                        variant="outlined"
-                        placeholder="URL da foto do produto"
-                        onChange={ (e) => setPhotos(e.target.value) }
-                    />
-                </Grid>
-            
-                <Grid item xs={12}>
-                    <TextField value={installments}
-                        fullWidth
-                        style={{ margin: 8 }}
-                        variant="outlined"
-                        placeholder="Forma de parcelamento"
-                        onChange={ (e) => setInstallments(e.target.value) }
-                    />
-                </Grid>
+        <Grid item xs={12}>
+          <TextField
+            value={installments}
+            fullWidth
+            style={{ margin: 8 }}
+            variant="outlined"
+            placeholder="Forma de parcelamento"
+            onChange={(e) => setInstallments(e.target.value)}
+          />
+        </Grid>
 
-                <Grid container
-                    direction="row"
-                    justify="flex-end"
-                    alignItems="flex-end"
-                >
-                    <Button variant="outlined" color="secondary" size="small"
-                        onClick={ClearAll}> 
-                            Limpar
-                    </Button>
+        <Grid
+          container
+          direction="row"
+          justify="flex-end"
+          alignItems="flex-end"
+        >
+          <Button
+            variant="outlined"
+            color="secondary"
+            size="small"
+            onClick={ClearAll}
+          >
+            Limpar
+          </Button>
 
-                    <Button variant="contained" color="primary" size="small"
-                        onClick={CreateProduct} className={classes.margin}>
-                            Criar Produto
-                    </Button>
-                </Grid>
-            </Formulario>
-        </Container>
-    )
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            onClick={CreateProduct}
+            className={classes.margin}
+          >
+            Criar Produto
+          </Button>
+        </Grid>
+      </Formulario>
+    </Container>
+  );
 }
