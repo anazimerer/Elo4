7dfaee6094f051aa81e9797873f179d5c41501d0
Ana Zimerer <ana.zimerer@gmail.com>
2020-06-11 19:46:55 -0300

    Estrutura

--- src/Pages/Carrinho/index.js
+++ src/Pages/Carrinho/index.js
@@ -25,8 +25,9 @@ const SectionInfos = styled.section`
   height: 100%;
   div {
     width: 100%;
-    height: 40vh;
-    border: 1px solid black;
+    height: 25vh;
+    border: 1px solid grey;
+	margin: 10px;
   }
   h3{
 	  margin: 5px;
@@ -39,16 +40,21 @@ const SectionPayment = styled.section`
   flex-direction: column;
   width: 30%;
   height: 100%;
+  }
   div {
     width: 100%;
-    height: 15vh;
-    border: 1px solid black;
-  }
+    height: 7vh;
+    border: 1px solid grey;
+	margin: 10px;
+  }  
   h3{
 	  margin: 5px;
 	  padding: 3px;
   }
-`;
+`
+const InfosPayment = styled.div`
+	height: 15vh !important;
+`
 const DivPhoto = styled.div`
   width: 50px;
   height: 55px;
@@ -56,19 +62,28 @@ const DivPhoto = styled.div`
     width: 100%;
     height: 100%;
   }
-`;
+`
 const ProductCart = styled.div`
   display: flex;
   flex-direction: row;
-  border: 1px solid black;
-`;
+  border: 1px solid grey;
+  margin: 10px;
+`
+
 const ProductInfo = styled.div`
   padding: 2px;
   h4,
   p {
     margin: 0;
   }
+`
+const DivRadio = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 10%;
 `;
+
 function Carrinho() {
   return (
     <DivRoot>
@@ -98,16 +113,34 @@ function Carrinho() {
 		</SectionCart>
 
       <SectionInfos>
-        <h3>informaçoes</h3>
-        <div>endereço de entrega</div>
-        <div>frete</div>
+        <h3>Informações</h3>
+        <div>
+			<h4>endereço de entrega</h4>			
+			<p>Avenida Brasil, 556</p>
+			<p>Centro - 02336-070</p>
+			<p>São Paulo - SP</p>
+			
+			<a>Alterar endereço</a>
+			<hr />		
+        Frete
+    	<input type="radio" value="0" name="frete-padrao"/>
+    	<label for="campo-radio1">Padrão (R$12,80)</label>
+    	<input type="radio" value="0" name="frete-gratis"/>
+    	<label for="campo-radio2">Reduzido (grátis) </label>		
+		</div>
       </SectionInfos>
 
       <SectionPayment>
         <h3>Pagamento</h3>
-        <div>produtos/ preço total frete taxa de serviço cupom</div>
+        <InfosPayment>
+			<h4>produtos</h4>
+			<p>preço total </p>
+			<p>frete</p>
+			<p>taxa de serviço </p>
+			<p>cupom</p>
+		</InfosPayment>
 
-        <div>total a pagar</div>
+        <h3>total a pagar:</h3>
 
         <section>
           <div>Paypal</div>
