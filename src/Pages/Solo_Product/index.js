3da2ba352c2149457b4fc8cb027e1b4d05d6666a
Ronaldo Jonson <ronaldojonson@hotmail.com>
2020-06-12 06:08:32 -0300

    product-screen-style

--- src/Pages/Solo_Product/index.js
+++ src/Pages/Solo_Product/index.js
@@ -1,98 +1,147 @@
-import React, {useEffect} from 'react';
-import api from '../../service/api'
+import React, { useEffect } from 'react';
+import api from '../../service/api';
+import {Link} from 'react-router-dom'
 
-import { Container, Box, Button, Card, Grid, styled } from '@material-ui/core'
-import { Panel, DetailsBox, TitleBox, MainProductImage, ProductImage } from './styles'
+import { Container } from './styles';
+import { Panel, NonHeightPanel, AvatarPanel, DataTable } from './styles';
+import { MainImage, SecondaryImage } from './styles';
+import { BuyPanel } from './styles';
 
-function Solo_Product() {
-    const [product, setProduct] = React.useState(
-    
-    );
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import BeenhereIcon from '@material-ui/icons/Beenhere';
+import RemoveShoppingCartIcon from '@material-ui/icons/RemoveShoppingCart';
+import ReportProblemIcon from '@material-ui/icons/ReportProblem';
 
-    useEffect(() => {
-        GetProductlist();
-    }, []);
+function Solo_Product({
+  match: {
+    params: { id },
+  },
+}) {
+  const [product, setProduct] = React.useState();
 
-    const GetProductlist = () => {
-        api.get()
-        .then((response) => {
-            console.log(response.data.products[0]);
-            setProduct(response.data.products[0]);
-        })
+  useEffect(() => {
+    if(JSON.parse(localStorage.getItem("cart") === null)){
+        localStorage.setItem("cart", JSON.stringify([]));
     }
 
-    const GetProductInfo = () => {
-        if(product !== undefined){
-            return (
-            <DetailsBox id="teste">
-                <TitleBox>
-                    <h2>{product.name}</h2>
-                    <p>{product.category}</p>
-                </TitleBox>
-                <div>
-                    <p>{product.description}</p>
-                </div>
-                <div>
-                    <img src="https://picsum.photos/20/20"/>
+    GetProductlist();
+  }, []);
+
+  const GetProductlist = () => {
+    api.get().then((response) => {
+        console.log(response);
+        response.data.products.forEach(product => {
+            if(product.id === "eZWLnhldjC8tg5ImsmFC")
+            {
+                setProduct(product);
+            }
+        });
+    });
+  };
+
+  const AddToCart = (product) =>{
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    cart.push(product);
+    console.log(cart);
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+
+  const GetProductInfo = () => {
+    if (product !== undefined) {
+      return (
+        <div>
+          <Panel>
+            <div>
+                <h2>{product.name}</h2>
+                <p>{product.category}</p>
+            </div>
+          </Panel>
+          <Panel>
+            <div>
+                <p>{product.description}</p> 
+            </div>
+          </Panel>
+          <Panel>
+            <div>
+                <AvatarPanel>
+                    <Avatar src="https://picsum.photos/20/20" />
                     <p>Leticia</p>
                     <p>Araraquara, SP</p>
-                    <Button variant="contained" color="primary">Seguir Loja</Button>
-                </div>
-                <div>
-                    <p>{product.installments}</p>
-                    <p>Disponiveis</p>
-                    <p>80</p>
-                    <p>Vendidos</p>
-                    <p>15</p>
-                    <p>Recomendações</p>
-                </div>
-            </DetailsBox> 
-            );
-        }
-    }
+                </AvatarPanel>
+                <Button variant="outlined" color="primary" fullWidth='true' >Seguir Loja</Button>
+            </div>
+          </Panel>
+          <NonHeightPanel>
+            <DataTable>
+                <table>
+                    <tr>
+                        <td><p>{product.installments}</p></td>
+                        <td><p>80</p></td>
+                        <td><p>15</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>Á venda</p></td>
+                        <td><p>Vendidos</p></td>
+                        <td><p>Comentarios</p></td>
+                    </tr>
+                </table>
+            </DataTable>
+          </NonHeightPanel>
+        </div>
+      );
+    }   
+  };
 
-    const GetPhotos = () => {
-        if(product !== undefined){
-            return <Box>
-                <MainProductImage src="https://picsum.photos/1000/1000"/>
-                <div>
-                    <ProductImage src="https://picsum.photos/1000/1000"/>
-                    <ProductImage src="https://picsum.photos/1000/1000"/>
-                    <ProductImage src="https://picsum.photos/1000/1000"/>
-                    <ProductImage src="https://picsum.photos/1000/1000"/>
-                </div>
-            </Box> 
-        }
+  const GetPhotos = () => {
+    if (product !== undefined) {
+      return (
+        <div>
+          <MainImage src="https://picsum.photos/200/200" />
+        </div>
+      );
     }
+  };
 
-    const GetPrice = () => {
-        if(product !== undefined){
-            return <Box>
-                <Card variant="outlined">
-                    <p>{product.price}</p>
-                    <Button variant="contained" color="primary" >Quero Comprar</Button>
-                    <Button variant="contained" color="primary" >Adicionar ao Carrinho</Button>
-                </Card>
-                <Card variant="outlined">
-                    <img src="https://picsum.photos/20/20"/>
-                    <p>Comprar na Elo4 é seguro</p>
-                    <img src="https://picsum.photos/20/20"/>
-                    <p>Se não gostar a devolução é gratis</p>
-                    <img src="https://picsum.photos/20/20"/>
-                    <p>Denunciar</p>
-                </Card>
-            </Box> 
-        }
+  const GetPrice = () => {
+    if (product !== undefined) {
+      return (
+        <div>
+          <br/><br/>
+          <Panel>
+            <BuyPanel>
+                <p>R${product.price}</p>
+                <Link to="/Payment">
+                    <Button variant="contained" color="primary" fullWidth="true">Quero Comprar</Button>
+                </Link>
+                <Button variant="contained" color="primary" fullWidth="true" onClick={() => AddToCart(1)}>Adicionar ao Carrinho</Button>
+            </BuyPanel>
+          </Panel>
+          <table>
+            <tr>
+              <td><BeenhereIcon/></td>
+              <td><p>Comprar na Elo4 é seguro</p></td>
+            </tr>
+            <tr>
+                <td><RemoveShoppingCartIcon/></td>
+                <td><p>Se não gostar a devolução é gratis</p></td>
+            </tr> 
+            <tr>
+                <td> <ReportProblemIcon/></td>
+                <td> <p>Denunciar</p> </td>
+            </tr>
+          </table>
+        </div>
+      );
     }
+  };
 
-  return ( 
-  <Container>
-      <Panel>
-        {GetProductInfo()}
-        {GetPhotos()}
-        {GetPrice()}
-      </Panel>
-  </Container>
+  return (
+    <Container>
+      {GetProductInfo()}
+      {GetPhotos()}
+      {GetPrice()}
+    </Container>
   );
 }
 
--- src/Pages/Solo_Product/styles.js
+++ src/Pages/Solo_Product/styles.js
@@ -1,34 +1,84 @@
 import styled from 'styled-components';
 
-export const Content = styled.div`
-
-`
-
-export const Panel = styled.div`
-    width: 80vw;
-    height: 50v;
-    margin-top: 10%;
-
+export const Container = styled.div`
+    margin: 5% 4.2% 5% 2%;
     display: flex;
-    flex-direction: row;
     justify-content: space-between;
-`;
+`
 
-export const DetailsBox = styled.div`
+export const Deafault = styled.div`
 
 `
 
-export const TitleBox = styled.div`
-    height: 5vh;
+// Left Panels
+export const Panel = styled.div`
+    min-height: 15vh;
+    border-style: solid;
+    border-color: grey;
+    border-width: 1px;
+    & > div{
+        margin: 10px 10px;
+    }
 `
 
-export const MainProductImage = styled.img`
-    width:40vw;
-    height:40vh;
+export const NonHeightPanel = styled.div`
+    border-style: solid;
+    border-color: grey;
+    border-width: 1px;
+    & > div{
+        margin: 10px 10px;
+    }
 `
 
-export const ProductImage = styled.img`
-    width:5vw;
-    height:5vh;
-    padding-right: 10px;
+export const AvatarPanel = styled.div`
+    display: grid;
+    grid-template-columns: 50px;
+    grid-template-rows:25px; 
+    & > img{
+        grid-row-start: 1;
+        grid-row-end:2;
+    }
+    & > p{
+        grid-column-start: 2;
+        margin-bottom: 15px;
+    }
+`
+
+export const DataTable = styled.div`
+    min-width: 15vw;
+    width: 100%;
+
+    & > table{
+        width: 100%;
+        margin: auto;
+
+        & > tr{
+            text-align: center;
+        }   
+        & > td, tr{
+            vertical-align: bottom;
+        } 
+    }
+`
+// Left Panels
+
+// Mid Panel
+export const MainImage = styled.img`
+    width: 400px;
+    height: 400px;
+`
+export const SecondaryImage = styled.img`
+    width: 40px;
+    height: 40px;
+    margin-right: 10px;
+`
+// Mid Panel
+
+// Right Panel
+export const BuyPanel = styled.div`
+    height: 20vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
 `
