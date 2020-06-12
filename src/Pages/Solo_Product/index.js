64fd92ed2b11647be175690ba94e62352410ba74
Jonatan Machado <jonatanmachado35@gmail.com>
2020-06-12 14:08:53 -0300

    bugs solo_products

--- src/Pages/Solo_Product/index.js
+++ src/Pages/Solo_Product/index.js
@@ -46,182 +46,158 @@ export default function Solo_Product({
     console.log(cart);
     localStorage.setItem('cart', JSON.stringify(cart));
   };
-
-  function Solo_Product({
-    match: {
-      params: { id },
-    },
-  }) {
-    const [product, setProduct] = React.useState();
-
-    useEffect(() => {
-      GetProductlist();
-    }, []);
-
-    const GetProductlist = () => {
-      api.get().then((response) => {
-        console.log(response.data.products[0]);
-        setProduct(response.data.products[0]);
-      });
-    };
-
-    const GetProductInfo = () => {
-      if (product !== undefined) {
-        return (
-          <div>
-            <Panel>
-              <div>
-                <h2>{product.name}</h2>
-                <p>{product.category}</p>
-              </div>
-            </Panel>
-            <Panel>
-              <div>
-                <p>{product.description}</p>
-              </div>
-            </Panel>
-            <Panel>
-              <div>
-                <AvatarPanel>
-                  <Avatar src="https://picsum.photos/20/20" />
-                  <p>Leticia</p>
-                  <p>Araraquara, SP</p>
-                </AvatarPanel>
-                <Button variant="outlined" color="primary" fullWidth="true">
-                  Seguir Loja
-                </Button>
-              </div>
-            </Panel>
-            <NonHeightPanel>
-              <DataTable>
-                <table>
-                  <tr>
-                    <td>
-                      <p>{product.installments}</p>
-                    </td>
-                    <td>
-                      <p>80</p>
-                    </td>
-                    <td>
-                      <p>15</p>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <p>Á venda</p>
-                    </td>
-                    <td>
-                      <p>Vendidos</p>
-                    </td>
-                    <td>
-                      <p>Comentarios</p>
-                    </td>
-                  </tr>
-                </table>
-              </DataTable>
-            </NonHeightPanel>
-          </div>
-        );
-      }
-
-      const GetPhotos = () => {
-        if (product !== undefined) {
-          return (
+  const GetProductInfo = () => {
+    if (product !== undefined) {
+      return (
+        <div>
+          <Panel>
             <div>
-              <MainImage src="https://picsum.photos/200/200" />
-
-              <img src="https://picsum.photos/200/200" />
-              <img src="https://picsum.photos/200/200" />
-              <img src="https://picsum.photos/200/200" />
-              <img src="https://picsum.photos/200/200" />
-              <img src="https://picsum.photos/200/200" />
+              <h2>{product.name}</h2>
+              <p>{product.category}</p>
             </div>
-          );
-        }
-      };
-
-      const GetPrice = () => {
-        if (product !== undefined) {
-          return (
+          </Panel>
+          <Panel>
             <div>
-              <br />
-              <br />
-              <Panel>
-                <BuyPanel>
-                  <p>R${product.price}</p>
-                  <Link to="/Payment">
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      fullWidth="true"
-                    >
-                      Quero Comprar
-                    </Button>
-                  </Link>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    fullWidth="true"
-                    onClick={() => AddToCart(1)}
-                  >
-                    Adicionar ao Carrinho
-                  </Button>
-                </BuyPanel>
-              </Panel>
+              <p>{product.description}</p>
+            </div>
+          </Panel>
+          <Panel>
+            <div>
+              <AvatarPanel>
+                <Avatar src="https://picsum.photos/20/20" />
+                <p>Leticia</p>
+                <p>Araraquara, SP</p>
+              </AvatarPanel>
+              <Button variant="outlined" color="primary" fullWidth="true">
+                Seguir Loja
+              </Button>
+            </div>
+          </Panel>
+          <NonHeightPanel>
+            <DataTable>
               <table>
                 <tr>
                   <td>
-                    <BeenhereIcon />
+                    <p>{product.installments}</p>
                   </td>
                   <td>
-                    <p>Comprar na Elo4 é seguro</p>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <RemoveShoppingCartIcon />
+                    <p>80</p>
                   </td>
                   <td>
-                    <p>Se não gostar a devolução é gratis</p>
+                    <p>15</p>
                   </td>
                 </tr>
                 <tr>
                   <td>
-                    {' '}
-                    <ReportProblemIcon />
+                    <p>Á venda</p>
                   </td>
                   <td>
-                    {' '}
-                    <p>Denunciar</p>{' '}
+                    <p>Vendidos</p>
+                  </td>
+                  <td>
+                    <p>Comentarios</p>
                   </td>
                 </tr>
               </table>
-
-              <div>
-                <p>{product.price}</p>
-                <button>Quero Comprar</button>
-                <button>Adicionar ao Carrinho</button>
-              </div>
-              <div>
-                <img src="https://picsum.photos/20/20" />
-                <p>Comprar na Elo4 é seguro</p>
-                <img src="https://picsum.photos/20/20" />
-                <p>Se não gostar a devolução é gratis</p>
-                <img src="https://picsum.photos/20/20" />
-                <p>Denunciar</p>
-              </div>
-            </div>
-          );
-        }
-      };
-
-      return (
-        <Container>
-          {GetProductInfo()}
-          {GetPhotos()}
-          {GetPrice()}
-        </Container>
+            </DataTable>
+          </NonHeightPanel>
+        </div>
       );
+    }
+
+    const GetPhotos = () => {
+      if (product !== undefined) {
+        return (
+          <div>
+            <MainImage src="https://picsum.photos/200/200" />
+
+            <img src="https://picsum.photos/200/200" />
+            <img src="https://picsum.photos/200/200" />
+            <img src="https://picsum.photos/200/200" />
+            <img src="https://picsum.photos/200/200" />
+            <img src="https://picsum.photos/200/200" />
+          </div>
+        );
+      }
     };
-  }
+
+    const GetPrice = () => {
+      if (product !== undefined) {
+        return (
+          <div>
+            <br />
+            <br />
+            <Panel>
+              <BuyPanel>
+                <p>R${product.price}</p>
+                <Link to="/Payment">
+                  <Button variant="contained" color="primary" fullWidth="true">
+                    Quero Comprar
+                  </Button>
+                </Link>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth="true"
+                  onClick={() => AddToCart(1)}
+                >
+                  Adicionar ao Carrinho
+                </Button>
+              </BuyPanel>
+            </Panel>
+            <table>
+              <tr>
+                <td>
+                  <BeenhereIcon />
+                </td>
+                <td>
+                  <p>Comprar na Elo4 é seguro</p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <RemoveShoppingCartIcon />
+                </td>
+                <td>
+                  <p>Se não gostar a devolução é gratis</p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  {' '}
+                  <ReportProblemIcon />
+                </td>
+                <td>
+                  {' '}
+                  <p>Denunciar</p>{' '}
+                </td>
+              </tr>
+            </table>
+
+            <div>
+              <p>{product.price}</p>
+              <button>Quero Comprar</button>
+              <button>Adicionar ao Carrinho</button>
+            </div>
+            <div>
+              <img src="https://picsum.photos/20/20" />
+              <p>Comprar na Elo4 é seguro</p>
+              <img src="https://picsum.photos/20/20" />
+              <p>Se não gostar a devolução é gratis</p>
+              <img src="https://picsum.photos/20/20" />
+              <p>Denunciar</p>
+            </div>
+          </div>
+        );
+      }
+    };
+
+    return (
+      <Container>
+        {GetProductInfo()}
+        {GetPhotos()}
+        {GetPrice()}
+      </Container>
+    );
+  };
 }
